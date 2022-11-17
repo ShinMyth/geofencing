43d4ccc011c9fc3055cc04de34fcf35bb05ebd52
Kei Credo <ShinMyth@gmail.com>
2022-11-17 15:48:23 +0800

    Keith (Functions - Ongoing) 2022-11-17

--- js/geofence.js
+++ js/geofence.js
@@ -1,12 +1,22 @@
 var map;
+var mapMarkers = [];
+var mapPolygons = [];
 
-function initMap(employeeLocation, officeLocation, officePolygonPoints) {
-    generateMap(employeeLocation);
-    generateMapMarkers(employeeLocation, officeLocation);
-    generateMapPolygon(employeeLocation, officePolygonPoints);
+var employeeLocation;
+var officeLocation;
+var officePolygonPoints;
+
+function initMap(employeeLocationValue, officeLocationValue, officePolygonPointsValue) {
+    employeeLocation = employeeLocationValue;
+    officeLocation = officeLocationValue;
+    officePolygonPoints = officePolygonPointsValue;
+
+    generateMap();
+    generateMapMarkers();
+    generateMapPolygon();
 }
 
-function generateMap(employeeLocation) {
+function generateMap() {
     map = new google.maps.Map(
         document.getElementById("map"),
         {
@@ -15,56 +25,73 @@ function generateMap(employeeLocation) {
             disableDefaultUI: true,
         }
     );
+
+    map.addListener("click", function (event) {
+        updateMap(event.latLng);
+    });
 }
 
+function updateMap(eventLatLng) {
+    employeeLocation = eventLatLng;
+    generateMapMarkers();
+    generateMapPolygon();
+}
 
-function generateMapMarkers(employeeLocation, officeLocation) {
-    new google.maps.Marker({
+function generateMapMarkers() {
+    for (var x = 0; x < mapMarkers.length; x++) {
+        mapMarkers[x].setMap(null);
+    };
+    mapMarkers = [];
+
+
+    var employeeMarker = new google.maps.Marker({
         position: employeeLocation,
         title: "Employee",
         icon: "assets/images/employee-marker.png",
-        map,
+        map: map,
     });
-    new google.maps.Marker({
+
+    mapMarkers.push(employeeMarker);
+
+    var officeMarker = new google.maps.Marker({
         position: officeLocation,
         title: "Office",
         icon: "assets/images/office-marker.png",
-        map,
+        map: map,
     });
+    
+    mapMarkers.push(officeMarker);
 }
 
-function generateMapPolygon(employeeLocation, officePolygonPoints) {
-    var result = checkOfficePolygonContainsLocation(employeeLocation, officePolygonPoints);
+function generateMapPolygon() {
+    var result = checkPolygonContainsLocation();
 
-    var officePolygon;
-    if (result) {
-        officePolygon = new google.maps.Polygon({
-            paths: officePolygonPoints,
-            strokeColor: "#0000FF",
-            strokeOpacity: 0.75,
-            strokeWeight: 1.5,
-            fillColor: "#0000FF",
-            fillOpacity: 0.375,
-        });
-    } else {
-        officePolygon = new google.maps.Polygon({
-            paths: officePolygonPoints,
-            strokeColor: "#FF0000",
-            strokeOpacity: 0.75,
-            strokeWeight: 1.5,
-            fillColor: "#FF0000",
-            fillOpacity: 0.375,
-        });
-    }
+    for (var i = 0; i < mapPolygons.length; i++) {
+        mapPolygons[i].setMap(null);
+    };
+    mapPolygons = [];
 
-    officePolygon.setMap(map);
+    var officePolygon = new google.maps.Polygon({
+        paths: officePolygonPoints,
+        fillColor: result ? "#00FF00" : "#000000" ,
+        fillOpacity: result ? 0.275 : 0.35,
+        strokeColor: result ? "#41495C" : "#F56F6C",
+        strokeWeight: 1,
+        map: map,
+    });
+
+    officePolygon.addListener('click', function (event) {
+        updateMap(event.latLng);
+    });
+
+    mapPolygons.push(officePolygon);
 }
 
 
-function checkOfficePolygonContainsLocation(employeeLocation, officePolygonPoints) {
-    const officePolygon = new google.maps.Polygon({ paths: officePolygonPoints });
+function checkPolygonContainsLocation() {
+    var officePolygon = new google.maps.Polygon({ paths: officePolygonPoints });
 
-    const containsLocation = google.maps.geometry.poly.containsLocation(
+    var containsLocation = google.maps.geometry.poly.containsLocation(
         employeeLocation,
         officePolygon,
     );
--- js/values.js
+++ js/values.js
@@ -1,14 +1,8 @@
-// Road infront of Jetti
-const employeeLocation1 = { lat: 8.494248946505653, lng: 124.65202668111144 };
+const employeeLocationValue = { lat: 8.494248946505653, lng: 124.65202668111144 };
 
-// He and Sons Building (E2E Office)
-const employeeLocation2 = { lat: 8.49473485297278, lng: 124.65163062919602 };
+const officeLocationValue = { lat: 8.494599601911714, lng: 124.65175666051282 };
 
-// He and Sons Building
-const officeLocation = { lat: 8.494599601911714, lng: 124.65175666051282 };
-
-// He and Sons Building
-const officePolygonPoints = [
+const officePolygonPointsValue = [
     { lat: 8.494447689033173, lng: 124.65176473964237 },
     { lat: 8.49468975564717, lng: 124.65185459364372 },
     { lat: 8.494777297507396, lng: 124.65161520650037 },
